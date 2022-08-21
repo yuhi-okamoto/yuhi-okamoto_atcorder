9e8c514523d8c7b8e9e5397986d69b91a58907c4
yuhi-okamoto <95659990+yuhi-okamoto@users.noreply.github.com>
2022-08-21 18:18:33 +0900

    264_A

---
+++ 264_A.js
@@ -0,0 +1,12 @@
+function main(input) {
+  const str = 'atcoder';
+  const args = input.split('\n');
+  const nums = args[0].split(' ');
+  const l = parseInt(nums[0], 10);
+  const r = parseInt(nums[1], 10);
+  const result = str.substr( l, r );
+
+  console.log(result);
+}
+
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
